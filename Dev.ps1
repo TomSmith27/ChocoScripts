95c210ed340fe28cb8e8d141d747d305017d8c37
TomSmith27 <tom.smith2711@googlemail.com>
2019-10-18 14:31:14 +0100

    Update Dev.ps1

--- Dev.ps1
+++ Dev.ps1
@@ -1,9 +1,10 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y googlechrome
 choco install -y firefox
 choco install -y 7zip
 choco install -y git
 choco install -y putty
-choco install -y nodejs
+choco install -y nodejs-lts
 choco install -y python
 choco install -y paint.net
 choco install -y dotnetcore-sdk
