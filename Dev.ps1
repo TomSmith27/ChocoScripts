ce0f04fbf83220562798eac51e4f1f541b9893fe
TomSmith27 <tom.smith2711@googlemail.com>
2019-10-28 15:33:14 +0000

    Update Dev.ps1

--- Dev.ps1
+++ Dev.ps1
@@ -5,12 +5,10 @@ choco install -y 7zip
 choco install -y git
 choco install -y putty
 choco install -y nodejs-lts
-choco install -y python
 choco install -y paint.net
 choco install -y dotnetcore-sdk
 choco install -y vscode
 choco install -y spotify
-choco install -y paint.net
 choco install -y git-fork
 choco install -y linqpad
 choco install -y slack
